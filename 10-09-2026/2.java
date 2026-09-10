80ea7fe08cffa6b26610ae2213874f8277364e2c
Jangala Megha Harsha <155617450+Jangala-Megha-Harsha@users.noreply.github.com>
2026-09-11 00:51:15 +0530

    Add files via upload

---
+++ 10-09-2026/2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+	public static long tree[];
+	public static long MOD = 1000000007;
+	
+	public static void build(int index, int st, int ed, long arr[]) {
+		if(st == ed) {
+			tree[index] = arr[st] % MOD;
+			return;
+		}
+		int mid = (st + ed) / 2;
+		build(2 * index + 1, st, mid, arr);
+		build(2 * index + 2, mid + 1, ed, arr);
+		tree[index] = (tree[2 * index + 1] * tree[2 * index + 2]) % MOD;
+	}
+	
+	public static void update(int index, int st, int ed, long arr[], int x, long val) {
+		if(st == ed) {
+			arr[x] = val;
+			tree[index] = val % MOD;
+			return;
+		}
+		int mid = (st + ed) / 2;
+		if(x <= mid) update(2 * index + 1, st, mid, arr, x, val);
+		else update(2 * index + 2, mid + 1, ed, arr, x, val);
+		tree[index] = (tree[2 * index + 1] * tree[2 * index + 2]) % MOD;
+	}
+	
+	public static long query(int index, int st, int ed, int l, int r) {
+		if(l > ed || r < st) return 1;
+		if(l <= st && ed <= r) return tree[index];
+		int mid = (st + ed) / 2;
+		return (query(2 * index + 1, st, mid, l, r) * query(2 * index + 2, mid + 1, ed, l, r)) % MOD;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long arr[] = new long[n];
+		for(int i = 0; i < n; i++) arr[i] = sc.nextLong();
+		tree = new long[4 * n];
+		build(0, 0, n - 1, arr);
+		int q = sc.nextInt();
+		for(int i = 0; i < q; i++) {
+			int type = sc.nextInt();
+			if(type == 1) {
+				int index = sc.nextInt();
+				long value = sc.nextLong();
+				update(0, 0, n - 1, arr, index, value);
+			} else {
+				int l = sc.nextInt();
+				int r = sc.nextInt();
+				System.out.println(query(0, 0, n - 1, l, r));
+			}
+		}
+	}
+}
---
+++ 10-09-2026/4.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+	public static int tree[];
+	
+	public static void build(int index, int st, int ed, int arr[]) {
+		if(st == ed) {
+			tree[index] = arr[st];
+			return;
+		}
+		int mid = (st + ed) / 2;
+		build(2 * index + 1, st, mid, arr);
+		build(2 * index + 2, mid + 1, ed, arr);
+		tree[index] = tree[2 * index + 1] + tree[2 * index + 2];
+	}
+	
+	public static void update(int index, int st, int ed, int arr[], int x, int val) {
+		if(st == ed) {
+			arr[x] = val;
+			tree[index] = val;
+			return;
+		}
+		int mid = (st + ed) / 2;
+		if(x <= mid) update(2 * index + 1, st, mid, arr, x, val);
+		else update(2 * index + 2, mid + 1, ed, arr, x, val);
+		tree[index] = tree[2 * index + 1] + tree[2 * index + 2];
+	}
+	
+	public static int kthOne(int index, int st, int ed, int k) {
+		if(st == ed) return st;
+		int mid = (st + ed) / 2;
+		if(tree[2 * index + 1] >= k)
+			return kthOne(2 * index + 1, st, mid, k);
+		return kthOne(2 * index + 2, mid + 1, ed, k - tree[2 * index + 1]);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+		tree = new int[4 * n];
+		build(0, 0, n - 1, arr);
+		int q = sc.nextInt();
+		for(int i = 0; i < q; i++) {
+			int type = sc.nextInt();
+			if(type == 1) {
+				int index = sc.nextInt();
+				int value = sc.nextInt();
+				update(0, 0, n - 1, arr, index, value);
+			} else {
+				int k = sc.nextInt();
+				if(tree[0] < k) System.out.println(-1);
+				else System.out.println(kthOne(0, 0, n - 1, k));
+			}
+		}
+	}
+}
